5a9a7e995ff071cb04f1e12f05245e26b71837ae
David <514394792@qq.com>
2019-06-05 18:12:23 +0800

    6月5日

--- Swift初见.playground/Contents.swift
+++ Swift初见.playground/Contents.swift
@@ -28,8 +28,8 @@ let myConstant = 42
 // 编译失败: change 'let' to 'var' to make it mutable（可变的）
 
 /*
- 1、let 声明常量，var 声明变量
- 2、声明常量必须初始化，而且只能被赋值一次
+     1、let 声明常量，var 声明变量
+     2、声明常量必须初始化，而且只能被赋值一次
  */
 
 
@@ -40,6 +40,7 @@ let implicitDouble = 70.0
 let explicitDouble : Double = 70.0
 
 let num1 : Float = 4
+print(num1)
 // let num2 : Int = 4.0
 /*
  能赋值给float类型的num1，为啥4.0赋值给Int类型的num2则报错
@@ -61,3 +62,38 @@ let num1 : Float = 4
 
 
 
+let label = "The width is"
+let width = 94
+//let widthLabel = label + width
+// error: binary operator '+' cannot be applied to operands of type 'String' and 'Int' - 错误:二进制操作符“+”不能应用于“String”和“Int”类型的操作数
+
+let widthLabel = label + " " + String(width)
+print(widthLabel)
+
+//    1、Swift变量/常量 值的数据类型永远不会自动转换（隐式转换）
+//    2、如需转换数据类型，请使用显式转换(强转)
+
+let apples = 3
+let oranges = 5
+//let testString = "\(apples)"
+//print(testString)
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+let age = 26.1
+
+let greeting = "Hello, I am \(age) years old"
+
+let quotation = """I said "I have \(apples) apples."
+
+       And then I said "I have \(apples + oranges) pices of fruit."
+"""
+print("quotation:\n \(quotation)")
+/*
+ multi-line string literal content must begin on a new line
+ 错误:多行字符串文字内容必须从新行开始
+ 
+ error: multi-line string literal closing delimiter must begin on a new line
+ 错误: 多行字符串文字结束分隔符必须从新行开始
+ 
+ */
